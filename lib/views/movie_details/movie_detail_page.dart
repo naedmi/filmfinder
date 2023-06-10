7958e184c02ca4dc8d54ccc478554f54f10a3fba
haasentimo <haasentimo@gmail.com>
2023-06-10 15:21:23 +0200

    #5 Filmübersicht * fixed that the Detail page does no longer show an error screen when fetching data from the API

--- lib/views/movie_details/movie_detail_page.dart
+++ lib/views/movie_details/movie_detail_page.dart
@@ -4,9 +4,11 @@ import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:remixicon/remixicon.dart';
 
-import '../../controllers/movie_details/movie_details_controller.dart';
-import '../../controllers/movie_details/movie_details_provider.dart';
-import '../common/navigation_widget_main_details.dart';
+import 'package:filmfinder/controllers/movie_details/movie_details_controller.dart';
+import 'package:filmfinder/controllers/movie_details/movie_details_provider.dart';
+import 'package:filmfinder/models/movie_details/movie_details.dart';
+import 'package:filmfinder/views/common/constants.dart';
+import 'package:filmfinder/views/common/navigation_widget_main_details.dart';
 
 class MovieDetailsPage extends ConsumerWidget {
   final String movieId;
@@ -19,79 +21,92 @@ class MovieDetailsPage extends ConsumerWidget {
 
     return SimpleBottomBarScaffold(
       showMiddleButton: false,
-      body: Container(
-        padding: const EdgeInsets.all(16.0),
-        child: ListView(
-          children: [
-            // Display the movie poster image
-            if (movieDetailsController.movieDetails.value?.posterPath != null)
-              CachedNetworkImage(
-                imageUrl: 'https://image.tmdb.org/t/p/w500${movieDetailsController.movieDetails.value?.posterPath}',
-                placeholder: (context, url) => const CircularProgressIndicator(),
-                errorWidget: (context, url, error) => const Icon(Remix.error_warning_line),
-              ),
-            const SizedBox(height: 16.0),
-            // Display the movie title
-            Center(
-              child: Text(
-                movieDetailsController.movieDetails.value!.title,
-                style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-              ),
-            ),
-            const SizedBox(height: 16.0),
-            //Display the genres as rounded boxes
-            Wrap(
-              spacing: 8.0,
-              runSpacing: 4.0,
-              children: movieDetailsController.movieDetails.value?.genres?.map((genre) {
-                return Container(
-                  padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
-                  decoration: BoxDecoration(
-                    color: Theme.of(context).colorScheme.primaryContainer,
-                    borderRadius: BorderRadius.circular(12.0),
-                  ),
-                  child: Text(genre['name']),
-                );
-              }).toList() ?? [],
-            ),
-            const SizedBox(height: 16.0),
-            // Display the movie star rating
-            Wrap(
-              spacing: 8.0,
-              children: [
-                Tooltip(
-                  message: '${movieDetailsController.movieDetails.value?.voteAverage} / 10 stars',
-                  child: RatingBarIndicator(
-                    itemSize: 20.0,
-                    rating: (movieDetailsController.movieDetails.value?.voteAverage ?? 0) / 2,
-                    itemCount: 5,
-                    itemBuilder: (BuildContext context, int index) {
-                      return Icon(
-                        Remix.star_fill,
-                        color: Theme.of(context).colorScheme.primary,
-                      );
-                    },
-                  ),
+      body: movieDetailsController.movieDetails.when(
+        data: (MovieDetails details) => Container(
+          padding: const EdgeInsets.all(16.0),
+          child: ListView(
+            children: [
+              // Display the movie poster image
+              if (details.posterPath != null)
+                CachedNetworkImage(
+                  imageUrl: 'https://image.tmdb.org/t/p/w500${details.posterPath}',
+                  placeholder: (context, url) => const CircularProgressIndicator(),
+                  errorWidget: (context, url, error) => const Icon(Remix.error_warning_line),
                 ),
-                Text(
-                  '${movieDetailsController.movieDetails.value?.voteCount} votes',
-                  style: const TextStyle(fontSize: 16.0),
+              const SizedBox(height: 16.0),
+              // Display the movie title
+              Center(
+                child: Text(
+                  details.title,
+                  style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                 ),
-              ],
-            ),
+              ),
+              const SizedBox(height: 16.0),
+              //Display the genres as rounded boxes
+              Wrap(
+                spacing: 8.0,
+                runSpacing: 4.0,
+                children: details.genres?.map((genre) {
+                  return Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).colorScheme.primaryContainer,
+                      borderRadius: BorderRadius.circular(12.0),
+                    ),
+                    child: Text(genre['name']),
+                  );
+                }).toList() ?? [],
+              ),
+              const SizedBox(height: 16.0),
+              // Display the movie star rating
+              Wrap(
+                spacing: 8.0,
+                children: [
+                  Tooltip(
+                    message: '${details.voteAverage} / 10 stars',
+                    child: RatingBarIndicator(
+                      itemSize: 20.0,
+                      rating: (details.voteAverage ?? 0) / 2,
+                      itemCount: 5,
+                      itemBuilder: (BuildContext context, int index) {
+                        return Icon(
+                          Remix.star_fill,
+                          color: Theme.of(context).colorScheme.primary,
+                        );
+                      },
+                    ),
+                  ),
+                  Text(
+                    '${details.voteCount} votes',
+                    style: const TextStyle(fontSize: 16.0),
+                  ),
+                ],
+              ),
 
-            const SizedBox(height: 16.0),
-            // Display the movie overview
-            Text(
-              movieDetailsController.movieDetails.value!.overview,
-              style: const TextStyle(fontSize: 16),
+              const SizedBox(height: 16.0),
+              // Display the movie overview
+              Text(
+                details.overview,
+                style: const TextStyle(fontSize: 16),
+              ),
+              const SizedBox(height: 16.0),
+              // Display the vote average and vote count
+              // Add more details as needed
+            ],
+          ),
+        ),
+        loading: () => const Center(child: CircularProgressIndicator()),
+        error: (Object error, StackTrace stackTrace) =>  Align(
+          alignment: Alignment.topCenter,
+          child: Card(
+            child: Padding(
+              padding: const EdgeInsets.all(padding),
+              child: Text('Could not load data: ${error.toString()}'),
             ),
-            const SizedBox(height: 16.0),
-            // Display the vote average and vote count
-          // Add more details as needed
-          ],
+          ),
         ),
       ),
+
     );
   }
 }
