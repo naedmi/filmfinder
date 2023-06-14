b9cc21922743e1ea668e81acbf1d6ee31cb2e94d
naedmi <namied@pm.me>
2023-06-14 06:39:38 +0200

    #22 Favoritenliste * add list model, list controller and persistence service for hive

---
+++ lib/controllers/list/list_controller.dart
@@ -0,0 +1,72 @@
+import 'package:filmfinder/models/common/movie_result.dart';
+import 'package:filmfinder/providers.dart';
+import 'package:filmfinder/services/list/local_persistence_service.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+abstract class ListController {
+  ListController(this.ref);
+
+  final AutoDisposeStateProviderRef<ListController> ref;
+  late final LocalPersistenceService _localPersistenceService;
+  late final _movieList;
+
+  List<MovieResult> getAllMovies();
+
+  String? getMovieDetails(String id);
+
+  void addMovie(MovieResult res);
+
+  void removeMovie(int id);
+
+  void clearAll();
+
+  bool contains(int id);
+}
+
+class ListControllerImpl extends ListController {
+  ListControllerImpl(AutoDisposeStateProviderRef<ListController> ref)
+      : super(ref) {
+    _localPersistenceService = ref.watch(providers.localPersistenceProvider);
+    _movieList = ref.watch(providers.movieListProvider);
+  }
+
+  @override
+  List<MovieResult> getAllMovies() {
+    return _localPersistenceService.getAllMovies();
+  }
+
+  @override
+  String? getMovieDetails(String id) =>
+      _localPersistenceService.getMovieDetails(id);
+
+  @override
+  void addMovie(MovieResult res) {
+    _localPersistenceService.addMovie(movie: res);
+    update();
+  }
+
+  @override
+  void removeMovie(int id) {
+    _localPersistenceService.removeMovie(id);
+    update();
+  }
+
+  @override
+  void clearAll() {
+    _localPersistenceService.clearAll();
+    update();
+  }
+
+  @override
+  bool contains(int id) => _localPersistenceService.contains(id);
+
+  void update() {
+    _movieList.state = _movieList.state.copyWith(movies: getAllMovies());
+  }
+
+  // TODO: add list ordering
+  List<MovieResult> sortMoviesByTitle(List<MovieResult> movies) {
+    // movies.sort((Movie a, Movie b) => a.title.compareTo(b.title));
+    return movies;
+  }
+}
---
+++ lib/services/list/local_persistence_service.dart
@@ -0,0 +1,50 @@
+import 'package:filmfinder/models/common/movie_result.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+
+abstract class LocalPersistenceService {
+  List<MovieResult> getAllMovies();
+
+  String? getMovieDetails(String id);
+
+  void addMovie({required MovieResult movie});
+
+  void removeMovie(int id);
+
+  void clearAll();
+
+  bool contains(int id);
+}
+
+class LocalPersistenceServiceHive implements LocalPersistenceService {
+  late final Box<MovieResult> _movies;
+
+  // static const String boxName = 'fav_movies';
+
+  LocalPersistenceServiceHive() {
+    _movies = Hive.box<MovieResult>('fav_movies');
+  }
+
+  @override
+  void clearAll() {
+    _movies.clear();
+  }
+
+  @override
+  List<MovieResult> getAllMovies() => _movies.values.toList();
+
+  @override
+  String? getMovieDetails(String id) => _movies.get(id)?.overview;
+
+  @override
+  void addMovie({required MovieResult movie}) {
+    _movies.put(movie.id, movie);
+  }
+
+  @override
+  void removeMovie(int id) {
+    _movies.delete(id);
+  }
+
+  @override
+  bool contains(int id) => _movies.containsKey(id);
+}
