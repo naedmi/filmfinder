7f8fda05ec602fb8be2bd59b1673eefee836753e
naedmi <namied@pm.me>
2023-06-14 06:42:54 +0200

    #22 Favoritenliste * delete list media widget and replace it with search result widget * add refs to search result widget, make it ConsumerWidget * add basic add/remove functionality on button press

--- lib/views/list/list_page.dart
+++ lib/views/list/list_page.dart
@@ -1,44 +1,37 @@
-import 'package:easy_localization/easy_localization.dart';
-import 'package:filmfinder/models/common/movie_result.dart';
+import 'package:filmfinder/controllers/list/list_controller.dart'
+    as list_controller;
+import 'package:filmfinder/providers.dart';
 import 'package:filmfinder/views/common/constants.dart';
 import 'package:filmfinder/views/common/navigation_widget.dart';
-import 'package:filmfinder/views/list/list_media_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../models/list/movie_list.dart';
+import '../search/search_result_widget.dart';
 
 class ListPage extends ConsumerWidget {
   const ListPage({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final list_controller.ListController listController =
+        ref.watch(providers.listControllerProvider);
+    final MovieList movieList = ref.watch(providers.movieListProvider);
+
     return MainBottomBarScaffold(
       appBar: AppBar(
-        title: Center(
-          child: const Text('list.header').tr(),
-        ),
+        title: const Center(child: Text('My List')),
         automaticallyImplyLeading: false,
       ),
       body: ListView.separated(
         physics: const BouncingScrollPhysics(),
         padding: const EdgeInsets.only(
             left: padding, right: padding, bottom: paddingBig * 2),
-        itemBuilder: (BuildContext context, int index) => const ListMediaWidget(
-            res: MovieResult(
-                adult: true,
-                backdropPath: null,
-                genreIds: null,
-                id: 0,
-                originalLanguage: null,
-                originalTitle: null,
-                overview: null,
-                popularity: null,
-                posterPath: null,
-                releaseDate: null,
-                title: 'Title',
-                video: null,
-                voteAverage: null,
-                voteCount: null)),
-        itemCount: 4,
+        itemBuilder: (BuildContext context, int index) =>
+            movieList.movies.isEmpty
+                ? const Center(child: Text('No movies added to list'))
+                : SearchResultWidget(res: movieList.movies[index]),
+        itemCount: listController.getAllMovies().length,
         separatorBuilder: (BuildContext context, int index) => const Divider(),
       ),
     );
--- lib/views/search/search_result_widget.dart
+++ lib/views/search/search_result_widget.dart
@@ -1,17 +1,26 @@
+import 'package:filmfinder/controllers/list/list_controller.dart'
+    as list_controller;
 import 'package:filmfinder/models/common/movie_result.dart';
+import 'package:filmfinder/models/li
+import 'package:filmfinder/providers.dart';
 import 'package:filmfinder/views/common/constants.dart';
 import 'package:filmfinder/views/common/result_poster_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:remixicon/remixicon.dart';
 
-class SearchResultWidget extends StatelessWidget {
+class SearchResultWidget extends ConsumerWidget {
   final MovieResult res;
 
   const SearchResultWidget({Key? key, required this.res}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final list_controller.ListController listController =
+        ref.watch(providers.listControllerProvider);
+    final MovieList movieList = ref.watch(providers.movieListProvider);
+
     return Padding(
       padding: const EdgeInsets.all(paddingTiny),
       child:
@@ -86,11 +95,42 @@ class SearchResultWidget extends StatelessWidget {
                           ),
                           const Spacer(),
                           Tooltip(
-                            message: 'Add to list',
+                            message: listController.contains(res.id)
+                                ? 'Remove from list'
+                                : 'Add to list',
                             child: ElevatedButton(
-                              onPressed: () {},
+                              onPressed: () {
+                                showDialog(
+                                  context: context,
+                                  builder: (BuildContext context) {
+                                    return AlertDialog(
+                                      title: const Text('Confirmation'),
+                                      content: Text(
+                                          'Remove ${res.title} from your list?'),
+                                      actions: <Widget>[
+                                        TextButton(
+                                          child: const Text('Cancel'),
+                                          onPressed: () {
+                                            listController.removeMovie(res.id);
+                                            Navigator.of(context)
+                                                .pop(); // Close the dialog
+                                          },
+                                        ),
+                                        TextButton(
+                                          child: const Text('Remove'),
+                                          onPressed: () {
+                                            listController.removeMovie(res.id);
+                                            Navigator.of(context)
+                                                .pop(); // Close the dialog
+                                          },
+                                        ),
+                                      ],
+                                    );
+                                  },
+                                );
+                              },
                               child: const Icon(
-                                Remix.heart_line,
+                                Remix.heart_fill,
                               ),
                             ),
                           ),
