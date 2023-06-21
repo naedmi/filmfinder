cbb79cf00f6fc28147542fa16b9dbeecd6faeb9a
Ostabo <osi.borkenhagen@gmail.com>
2023-06-21 12:42:24 +0200

    #hotfix * searchControllerProvider.notifier needs watch instead of read for some reason

--- lib/views/settings/settings_page.dart
+++ lib/views/settings/settings_page.dart
@@ -55,7 +55,7 @@ class Settings extends ConsumerWidget {
     final SettingsLanguageModel languageModel =
         ref.watch(providers.settingsLanguageControllerProvider);
     final search_controller.SearchController searchController =
-        ref.read(providers.searchControllerProvider.notifier);
+        ref.watch(providers.searchControllerProvider.notifier);
     const double localPadding = 8.0;
     return Padding(
       padding: const EdgeInsets.only(
